9760f3c1b5bdee2e8a9352c2bc318c8a59da1f28
Zauats <33497760+Zauats@users.noreply.github.com>
2017-12-09 17:27:53 +0300

    Update Homework 2.4.py

--- Homework 2.4.py
+++ Homework 2.4.py
@@ -6,32 +6,39 @@ files_directory = os.path.join(current_dir, migrations)
 
 files = os.listdir(files_directory)
 
-sql_file_list = list()
-for file in files:
-    file_list = file.split(".")
-    if 'sql' in file_list:
-        sql_file_list.append(file)
+
+def file_sql_list():
+    sql_file_list = list()
+    for file in files:
+        file_list = os.path.splitext(file)
+        if '.sql' in file_list:
+            sql_file_list.append(file)
+    return sql_file_list
 
 
-search_list_1 = set()
+sql_file_list = file_sql_list()
 password = input('придумайте пароль для выхода')
+
+
+def search_list():
+    search_list = list()
+    for file in sql_file_list:
+        with open(os.path.join(files_directory, file)) as f:
+            file_information = f.read()
+            if world in file_information:
+                search_list.append(file)
+    return search_list
+
+
 while True:
     world = input('введите слово, которое вы ищите или команду для выхода из программы')
     if world == password:
         break
     if __name__ == '__main__':
-        for file in sql_file_list:
-            with open(os.path.join(files_directory, file)) as f:
-                file_information = f.read()
-                if world in file_information:
-                    search_list_1.add(file)
+        search_list()
 
+    sql_file_list = search_list()
 
-    sql_file_list = set(sql_file_list)
-    sql_file_list = set(sql_file_list & search_list_1)
-    sql_file_list = list(sql_file_list)
-    print(sql_file_list)
-    print("Всего:",len(sql_file_list))
-    search_list_1.clear()
+    print("{}{}{} {}".format(sql_file_list, "\n","Всего файлов найдено:",  len(sql_file_list)))
 
 print("Надеюсь вы нашли то, что хотели")
