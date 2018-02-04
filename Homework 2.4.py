28af18c7159c00d2d146a26ad08d95925298ad41
Zauats <33497760+Zauats@users.noreply.github.com>
2018-02-04 14:51:48 +0300

    Update Homework 2.4.py

--- Homework 2.4.py
+++ Homework 2.4.py
@@ -20,9 +20,9 @@ def file_sql_list():
 
 
 
-def search_list(list_number):
+def search_list(Your_list):
     search_list = list()
-    for file in list_[list_number]:
+    for file in Your_list:
         with open(os.path.join(files_directory, file)) as f:
             file_information = f.read()
             if world in file_information:
@@ -33,7 +33,6 @@ def search_list(list_number):
 
 if __name__ == '__main__':
     sql_file_list = file_sql_list()
-    list_ = [sql_file_list]
 
     password = input('придумайте пароль для выхода')
     while True:
@@ -41,7 +40,7 @@ if __name__ == '__main__':
         if world == password:
             break
         else:
-            sql_file_list = search_list(0)
+            sql_file_list = search_list(sql_file_list)
 
         for file in sql_file_list:
             print("{}\n".format(file))
